5f5e7b2722a2754689471e364b069c50d83f4161
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-05-22 22:13:29 +0300

    add modal window

--- js/app.js
+++ js/app.js
@@ -66,6 +66,29 @@ function showTabsContent(b) {
     }
 }
 
+////////////////// modal window   ///////////////////////
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("mybtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+span.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target == modal){
+        modal.style.display = 'none';
+    }
+};
+
+
+
+
 
 
 
