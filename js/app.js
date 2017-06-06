db2e86ca2bb115c4c66b9064f9923590703ada55
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-06-06 20:06:49 +0300

    add 16 lesson . TO DO

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ document.getElementById('tabs').onclick= function (event) {
     var target = event.target;
 
     if (target.className == 'tab'){
-        
+
         for (var i =0; i< tab.length; i++){
             if (target == tab[i]){
                 showTabsContent(i);
@@ -232,6 +232,63 @@ function accord() {
     }
 }
 
+////////// to do /////////////////
+
+
+
+var myNodelist = document.getElementsByTagName('LI');
+    for(var i=0; i < myNodelist.length; i++){
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+
+var close = document.getElementsByClassName("close");
+for (var i=0; i < close.length; i++){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+var list = document.getElementsByClassName('listMark');
+for (var i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function(ev){
+        if(ev.target.tagName === 'LI'){
+            ev.target.classList.toggle('checked');
+        }
+    },false);
+}
+
+
+function newElement(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("youInput").value;
+    var t = document.createTextNode(inputValue);
+    if(inputValue === ''){
+        alert("Поле не может быть пустым!");
+    }else{
+        document.getElementById("myULL").appendChild(li);
+    }
+
+    document.getElementById("youInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i=0; i < close.length; i++){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
 
 
 
