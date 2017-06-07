7697f92bf9c480ccf52b67b053c9c3402f0a28f5
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-06-07 17:51:16 +0300

    add 20 lesson . Snack bar

--- js/app.js
+++ js/app.js
@@ -290,6 +290,17 @@ function newElement(){
     }
 }
 
+/*-------------
+ Snack
+-------------*/
+
+document.getElementById('mybtn').onmouseover = function(event){
+    var x = document.getElementById('snackbar');
+    x.className = "show";
+    setTimeout(function () {
+        x.className = x.className.replace('show',"");
+    },3000);
+};
 
 
 
