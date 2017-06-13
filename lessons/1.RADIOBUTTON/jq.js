36d271c37bc3e9311b784a81aeec3ac101aaaeab
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-06-13 12:06:03 +0300

    fixed 2 lesson . JQ. working.

--- lessons/1.RADIOBUTTON/jq.js
+++ lessons/1.RADIOBUTTON/jq.js
@@ -1,24 +1,11 @@
 $('button').on('click', function() {
-    $(':input:radio').each(function (index,data) {
-        if ($('input:radio:checked')) {
-            alert('Выбран ' + index + ' элемент!');
-            }
-    })
-}); 
+    var radi = $(':input:radio');
 
-
-
-
-
-
-    // var clicked = $('input[type=radio]');
-
-    // for (var i = 0; i < clicked.length; i++){
-    //
-    //     if(clicked.checked){
-    //
-    //         alert ('Выбран ' + i + ' элемент!');
-    //     }
-    // }
+    for (var i = 0; i < radi.length; i++){
+        if(radi[i].checked){
+            alert ('Выбран ' + i + ' элемент!');
+        }
+    }
+});
 
 
